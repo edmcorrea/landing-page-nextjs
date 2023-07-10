273a439c2f623e8be6a05d68a7eec1283372590e
edcorrea <edm.correa@hotmail.com>
2023-07-10 12:24:33 -0300

    fix: Optimaze fetch on forms - frontend side

--- howhow-project/src/app/services/RequestMail/index..tsx
+++ howhow-project/src/app/services/RequestMail/index..tsx
@@ -1,10 +1,13 @@
-import axios from "axios";
-
-export const api = axios.create({
-  baseURL: 'http://localhost:3001',
-});
+const baseURL = 'http://localhost:3001';
 
 export const requestMail = async (endpoint: string, body: IContact) => {
-  const { data } = await api.post(endpoint, body);
-  return data;
+  const response = await fetch(`${baseURL}${endpoint}`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  })
+  await response.json()
 };
